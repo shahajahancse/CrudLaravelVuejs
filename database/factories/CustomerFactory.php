349d54f67805a552c0ba6e1e92bcc237d51a7a65
shahajahancse <mdrezaali1@gmail.com>
2019-10-30 20:44:08 +0600

    customer table migrate and faker data insert

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Customer::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'email' => $faker->unique()->freeEmail,
+        'phone' => $faker->tollFreePhoneNumber,
+        'address' => $faker->streetAddress,
+        'total' => $faker->randomFloat($nbMaxDecimals = 2, $min = 100.00, $max = 1000.00),
     ];
 });
--- database/migrations/2019_10_30_124212_create_customers_table.php
+++ database/migrations/2019_10_30_124212_create_customers_table.php
@@ -15,6 +15,11 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name', 128);
+            $table->string('email', 128);
+            $table->string('phone', 16);
+            $table->text('address');
+            $table->decimal('total', 8, 2)->default(000.00);
             $table->timestamps();
         });
     }
