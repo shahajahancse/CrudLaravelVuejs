4dc0841099063571c5cb2082bb38f687dadb9552
shahajahancse <mdrezaali1@gmail.com>
2019-10-31 19:04:03 +0600

    view all data CustomerCollection

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\CustomerCollection;
 use App\Customer;
 
 class CustomerController extends Controller
@@ -15,7 +16,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return Customer::all();
+        return new CustomerCollection(Customer::latest()->paginate(10));
     }
 
     /**
