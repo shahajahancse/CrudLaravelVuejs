3408602141edaddb549fd0ff207010d3f0fa99f4
shahajahancse <mdrezaali1@gmail.com>
2019-10-31 21:32:46 +0600

    specific data collection show

--- app/Http/Resources/CustomerCollection.php
+++ app/Http/Resources/CustomerCollection.php
@@ -14,6 +14,16 @@ class CustomerCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection->transform(function($customer){
+                return[
+                    'id' => $customer->id,
+                    'name' => $customer->name,
+                    'email' => $customer->email,
+                    'phone' => $customer->phone,
+                    'total' => $customer->total,
+                ];
+            })
+        ];
     }
 }
